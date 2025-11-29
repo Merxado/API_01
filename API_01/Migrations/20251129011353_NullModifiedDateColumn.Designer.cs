24f9ce2e4d97bf436cdc682d3e46dd7640689bad
Merxado <mayimamiyyo1112@gmail.com>
2025-11-28 20:18:05 -0500

    Correcion de la columna ModifiedDate not null.

--- API_01/Migrations/20251129011353_NullModifiedDateColumn.Designer.cs
+++ API_01/Migrations/20251129011353_NullModifiedDateColumn.Designer.cs
@@ -4,6 +4,7 @@ using API_01.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API_01.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251129011353_NullModifiedDateColumn")]
+    partial class NullModifiedDateColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
